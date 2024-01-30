b95be1f53a272c3cae122ef7d6a96ee3a0457d0f
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-30 17:06:11 +0530

    login

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { validateRegisterData } = require("../Utils/AuthUtils");
 const User = require("../Models/UserModel");
 const AuthRouter = express.Router();
+const bcrypt = require("bcrypt");
 
 AuthRouter.post("/register", async (req, res) => {
   const { name, email, username, password } = req.body;
@@ -9,6 +10,11 @@ AuthRouter.post("/register", async (req, res) => {
     await validateRegisterData({ name, email, username, password });
   } catch (error) {
     res.send({ status: 400, message: error });
+  }
+  try {
+    await User.usernameAndEmailExists({ email, username });
+  } catch (error) {
+    return res.send({ status: 400, message: "invalid credentials", error });
   }
   try {
     const userObj = new User({ name, email, username, password });
@@ -19,7 +25,20 @@ AuthRouter.post("/register", async (req, res) => {
       data: userDb,
     });
   } catch (error) {
-    return res.send({status:500, message: "database error", error})
+    return res.send({ status: 500, message: "database error", error });
+  }
+});
+
+AuthRouter.post("/login", async (req, res) => {
+  const { loginId, password } = req.body;
+  if(!loginId || !password) return res.send({status: 400, message:"missing credentials"})
+  try {
+    const userDb = await User.findUserWithLoginId({loginId})
+    const isMatched = await bcrypt.compare(password, userDb.password)
+    if(isMatched) return res.send({status:200, message:'login success'})
+    return res.send({status:400, message:'wrong password'})
+  } catch (error) {
+    return res.send(error)
   }
 });
 
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,4 +1,5 @@
 const UserSchema = require("../Schema/UserSchema");
+const bcrypt = require("bcrypt");
 
 let User = class {
   constructor({ name, email, username, password }) {
@@ -9,11 +10,15 @@ let User = class {
   }
   register() {
     return new Promise(async (resolve, reject) => {
+      const hashedPassword = await bcrypt.hash(
+        this.password,
+        Number(process.env.SALT)
+      );
       const userObj = new UserSchema({
         name: this.name,
         email: this.email,
         username: this.username,
-        password: this.password,
+        password: hashedPassword,
       });
       try {
         const userDb = await userObj.save();
@@ -23,7 +28,39 @@ let User = class {
       }
     });
   }
+  static usernameAndEmailExists({ email, username }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userExists = await UserSchema.findOne({
+          $or: [{ email }, { username }],
+        });
+        if (userExists && userExists.email === email)
+          reject("User with this email already exists");
+        if (userExists && userExists.username === username)
+          reject("User with this username already exists");
+        resolve();
+      } catch (error) {
+        reject({ status: 500, message: "database error" });
+      }
+    });
+  }
+  static findUserWithLoginId({ loginId }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userExists = await UserSchema.findOne({
+          $or: [{ email: loginId }, { username: loginId }],
+        });
+        if (!userExists)
+          reject({
+            status: 400,
+            message: "this email/username is not registered",
+          });
+        resolve(userExists);
+      } catch (error) {
+        reject({ status: 500, message: "database error" });
+      }
+    });
+  }
 };
 
-
-module.exports = User
+module.exports = User;
